1ee88b44c8c4a40b0cbdcff7e4632a887590da33
Christoph Dähne <christoph.daehne@sandstorm-media.de>
2013-09-17 10:37:40 +0200

    WIP bugfixes

--- manifests/host.pp
+++ manifests/host.pp
@@ -22,23 +22,24 @@ define sshuserconfig::host(
 	$entry_dest = "${ssh_dir}/config.d/${alias}"
 
 	$content = "Host ${alias}"
-	unless $remote_hostname == undef
-		$content += "HostName ${remote_hostname}"
-	unless $remote_port == undef
-		$content += "Port ${remote_port}"
-	unless $remote_username == undef
-		$content += "User ${remote_username}"
-	unless $priv_key_name == undef
-		$content += "IdentityFile ${ssh_dir}/${priv_key_name}"
+	unless $remote_hostname == undef {
+		$content += "\n\tHostName ${remote_hostname}"
+	}
+	unless $remote_port == undef {
+		$content += "\n\tPort ${remote_port}"
+	}
+	unless $remote_username == undef {
+		$content += "\n\tUser ${remote_username}"
+	}
+	unless $priv_key_name == undef {
+		$content += "\n\tIdentityFile ${ssh_dir}/${priv_key_name}"
+	}
+	$content += "\n\n"
 	
 	file { "${title}_${entry_dest}" :
 		path => $entry_dest,
 		ensure => 'present',
-		content => "Host ${alias}
-			HostName ${remote_hostname}
-			Port ${remote_port}
-			User ${remote_username}
-			IdentityFile ${privkey_path}\n\n",
+		content => $content,
 		notify => Exec['sshuserconfig_generate_config'],
 	}
 }
