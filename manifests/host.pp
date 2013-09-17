07dd8c0fbd59edd37b147f72e101475a4fe10e9f
Christoph Dähne <christoph.daehne@sandstorm-media.de>
2013-09-17 10:50:09 +0200

    Combined different config variables to on array

--- manifests/host.pp
+++ manifests/host.pp
@@ -1,9 +1,5 @@
 define sshuserconfig::host(
-  $remote_hostname = undef,
-  $remote_port = undef,
-  $remote_username = undef,
-  $priv_key_name = undef,
-  $user = undef,
+  $configLines
 ) {
 	#determine were to store the entry (for which user)
 	$unix_user = $user ? {
@@ -20,26 +16,11 @@ define sshuserconfig::host(
 	$ssh_dir = "/Users/${unix_user}/.ssh"
 	$alias = "${title}"
 	$entry_dest = "${ssh_dir}/config.d/${alias}"
-
-	$content = "Host ${alias}"
-	unless $remote_hostname == undef {
-		$content += "\n\tHostName ${remote_hostname}"
-	}
-	unless $remote_port == undef {
-		$content += "\n\tPort ${remote_port}"
-	}
-	unless $remote_username == undef {
-		$content += "\n\tUser ${remote_username}"
-	}
-	unless $priv_key_name == undef {
-		$content += "\n\tIdentityFile ${ssh_dir}/${priv_key_name}"
-	}
-	$content += "\n\n"
 	
 	file { "${title}_${entry_dest}" :
 		path => $entry_dest,
 		ensure => 'present',
-		content => $content,
+		content => "Host ${alias}" + join($configLines, "\n\t"),
 		notify => Exec['sshuserconfig_generate_config'],
 	}
 }
